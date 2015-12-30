d53633cc6f48c587fb812e5fd4415a4e10f5708f
vina7 <van7@pitt.edu>
2015-12-29 19:30:33 -0500

    Replaced all the  || and && with or and and for correct syntax. Changed the old minisumo.py to minisumo_CV.py

--- minisumo.py
+++ minisumo.py
@@ -1,47 +1,139 @@
-import numpy as np
-import cv2
+import time
 
-cap = cv2.VideoCapture(0)
+#load files
+execfile("lineSense2.py")
+execfile("longrangemethod.py")
+execfile("minisumo.py")
+execfile("minisumo_motorcontrol	2.py")
+execfile("mouse1.py")
+execfile("rangesensor.py")
+execfile("short_range_Sense.py")
+execfile("shortrangemethod.py")
 
-# take first frame of the video
-ret,frame = cap.read()
+#Create objects	and variables
+lineSensors= lineSensor_Class()
+motors= Motors_Class()
+longrange= longrange_Class()
+shortrange= shortrange_Class()
+mouse_data = mouse_pos(stat,x,y)
+first = 'y'
+origin = 'y'
 
-# setup initial location of window
-r,h,c,w = 250,150,400,150  # simply hardcoded the values
-track_window = (c,r,w,h)
+##can move this function into the lineSensors class
+def checkLines():
+	if lineSensors.check1() == 1:
+		motors.motor_move('x',0)
+		motors.motor_move('a', 4)
+		detect = 1
+	elif lineSensors.check2() == 1:
+		motors.motor_move('x',0)
+		motors.motor_move('a', 4)
+		detect = 1
+	elif lineSensors.check3() == 1:
+		motors.motor_move('x',0)
+		motors.motor_move('a', 4)
+		detect = 1
+	elif lineSensors.check4() == 1:
+		motors.motor_move('x',0)
+		motors.motor_move('a', 4)
+		detect = 1
+	else:
+		detect = 0
+	return detect
+	
+def loop2():  #used in sense line origin part of flow chart
+	while True: 
+		if mouse_data[0] > 0 or mouse_data[1] >0:
+			lines = checkLines
+			if lines == 0:
+				#all of these checks seem to lead to the same action in the flow chart?
+				if mouse_data[1] > 0  and mouse_data[0] > 0:
+					motors.motor_move('x',0)
+					motors.motor_move('a', 2)
+				elif mouse_data[0] < 0 and mouse_data[1] > 0:
+					motors.motor_move('x',0)
+					motors.motor_move('a', 2)
+				elif mouse_data[0] > 0 and mouse_data[1] < 0:
+					motors.motor_move('x',0)
+					motors.motor_move('a', 2)
+				elif mouse_data[0] < 0 and mouse_data[1] < 0:
+					motors.motor_move('x',0)
+					motors.motor_move('a', 2)
+				else:
+					break 
+			else:
+				motors.motor_move('x',0)
+				motors.motor_move('a', 4)
+		else:
+			break 
 
-# set up the ROI for tracking
-roi = frame[r:r+h, c:c+w]
-hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
-
-
-# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-
-while(1):
-    ret ,frame = cap.read()
-
-    if ret == True:
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-
-        # apply meanshift to get the new location
-        ret, track_window = cv2.meanShift(dst, track_window, term_crit)
-
-        # Draw it on image
-        x,y,w,h = track_window
-        img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
-        cv2.imshow('img2',img2)
-
-        k = cv2.waitKey(60) & 0xff
-        if k == 27:
-            break
-
-    else:
-        break
-
-cv2.destroyAllWindows()
-cap.release()
+##MAIN LOOP
+while True:
+	short = shortrange.rngsens
+	while short > 2 and short < 15:
+	##the part in the if should be added to the motor_move method so it doesn't need repeated
+		if first == 'y':
+			motors.motor_move('x',0)
+			time.sleep(1)
+			first = 'n'
+		else:
+			motors.motor_move('w',4)
+			origin = 'n'
+			lines = checkLines()
+	long = longrange.rangesens
+	if long > 10 and long < 80:
+		##not sure what the computer vision methods look like
+		##this is an outline
+		if cv == 1: #> 300
+			motors.motor_move('x',0)
+			time.sleep(1)
+			motors.motor_move('a',4)
+			if mouse_data[0] < 0:
+				if mouse_y > 0:
+					origin = 'n'
+					break
+				else:
+					motors.motor_move('x',0)
+					motors.motor_move('w',4)
+			if mouse_data[0] > 0:
+				motors.motor_move('x',0)
+				motors.motor_move('d',4)
+		elif cv == 0: #<300
+			motors.motor_move('x',0)
+			motors.motor_move('d',4)
+			if mouse_data[0] > 0:
+				motors.motor_move('x',0)
+				motors.motor_move('d',4)
+			else:
+				motors.motor_move('x',0)
+				motors.motor_move('a',4)
+	lines = checkLines()
+	if origin == 'y':
+		if lines == 0:
+			while True:
+				if long > 10 and long < 80:
+					break
+				else:
+				#this part needs work
+					motors.motor_move('x',0)
+					motors.motor_move('a',4)
+					time.sleep(6) ##check time (sec) for 120 degrees
+					motors.motor_move('x',0)
+					loop2()
+					motors.motor_move('d',0)
+					time.sleep(4) #check time for 60 degrees
+					loop2()
+		else:
+			while True:
+				if long > 10 and long < 80:
+					break
+				else:
+					degrees = 0
+					while degrees <= 360:
+						motors.motor_move('x',0)
+						motors.motor_move('d',4)
+						time.sleep(2) ##check how long it takes to turn 10 degrees
+						loop2()
+						degrees+=10
+					break 
+	lines = checkLines()
