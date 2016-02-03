7f386d23df5299f6279279b719e832a980dfc27d
zltoigo <zoe.toigo@pitt.edu>
2016-02-02 20:43:24 -0500

    Update Adafruit_LSM303.py

--- Adafruit_LSM303.py
+++ Adafruit_LSM303.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
-#Python library for Adafruit Flora Accelerometer/Compass Sensor (LSM303).
+	#!/usr/bin/python
+
+# Python library for Adafruit Flora Accelerometer/Compass Sensor (LSM303).
 # This is pretty much a direct port of the current Arduino library and is
 # similarly incomplete (e.g. no orientation value returned from read()
 # method).  This does add optional high resolution mode to accelerometer
@@ -27,6 +28,7 @@
 
 from Adafruit_I2C import Adafruit_I2C
 
+
 class Adafruit_LSM303(Adafruit_I2C):
 
     # Minimal constants carried over from Arduino library
@@ -51,15 +53,12 @@ class Adafruit_LSM303(Adafruit_I2C):
 
 
     def __init__(self, busnum=-1, debug=False, hires=False):
-        x=0
-	y=0
-	magx=0
-	magy=0
+
         # Accelerometer and magnetometer are at different I2C
         # addresses, so invoke a separate I2C instance for each
         self.accel = Adafruit_I2C(self.LSM303_ADDRESS_ACCEL, busnum, debug)
         self.mag   = Adafruit_I2C(self.LSM303_ADDRESS_MAG  , busnum, debug)
-	
+
         # Enable the accelerometer
         self.accel.write8(self.LSM303_REGISTER_ACCEL_CTRL_REG1_A, 0x27)
         # Select hi-res (12-bit) or low-res (10-bit) output mode.
@@ -92,23 +91,16 @@ class Adafruit_LSM303(Adafruit_I2C):
         # Read the accelerometer
         list = self.accel.readList(
           self.LSM303_REGISTER_ACCEL_OUT_X_L_A | 0x80, 6)
-        res = [( int(self.accel12(list, 0))/9.8,
-                 int(self.accel12(list, 2))/9.8,
-                 int(self.accel12(list, 4))/9.8 )]
-	global x
-	global y
-	x = (self.accel12(list, 0))/9.8
-	y = (self.accel12(list, 2))/9.8 
+        res = [( self.accel12(list, 0),
+                 self.accel12(list, 2),
+                 self.accel12(list, 4) )]
 
         # Read the magnetometer
         list = self.mag.readList(self.LSM303_REGISTER_MAG_OUT_X_H_M, 6)
         res.append((self.mag16(list, 0),
                     self.mag16(list, 2),
-                    self.mag16(list, 4))) # ToDo: Calculate orientation
-	global magx
-	global magy
-	magx = int(self.mag16(list, 0))
-	magy = int(self.mag16(list, 2))
+                    self.mag16(list, 4),
+                    0.0 )) # ToDo: Calculate orientation
 
         return res
 
@@ -117,63 +109,67 @@ class Adafruit_LSM303(Adafruit_I2C):
         self.mag.write8( LSM303_REGISTER_MAG_CRB_REG_M, gain)
 
 
+Simple example prints accel/mag data once per second:
+# if __name__ == '__main__':
+
+    # from time import sleep
+
+    # lsm = Adafruit_LSM303()
+
+    # print '[(Accelerometer X, Y, Z), (Magnetometer X, Y, Z, orientation)]'
+    # while True:
+        # print lsm.read()
+        # sleep(1) # Output is fun to watch if this is commented out
+
+
 # Simple example prints accel/mag data once per second:
-    def mag_accel(self):
-      from time import sleep
+def mag_accel():
+	from time import sleep
 
-      lsm = Adafruit_LSM303()
+	lsm = Adafruit_LSM303()
 
-      print '[(Accelerometer X, Y, Z), (Magnetometer X, Y, Z, orientation)]'
-      while True:
-	  lsm.read()
-	  axlast = x
-	  aylast = y
-	  mxlast = magx
-	  mylast = magy
-	  ax1=0
-	  ay1=0
-	  mx1 = 0
-	  my1 = 0
-	  axtotal=0
-	  aytotal=0
-	  mxtotal = 0
-	  mytotal = 0
-	  for i in range(1,5):
-	    sleep(.05)
-	    lsm.read()
-	    mx = magx
-	    my = magy
-	    mx1 = mx - mxlast
-	    my1 = my - mylast
-	    mxlast = mx
-	    mylast = my
-	    mxtotal = mxtotal + mx1
-	    mytotal = mytotal + my1
-	    ax=x 
-	    ay=y
-	    ax1 = ax-axlast
-	    ay1 = ay-aylast
-	    axlast = ax
-	    aylast = ay	    
-	    axtotal = axtotal + ax1
-	    aytotal = aytotal + ay1
-	  r_mx = mxtotal/5
-	  r_my = mytotal/5
-	  r_ax = axtotal/5
-	  r_ay = aytotal/5
-	  r_ax = int(r_ax)
-	  r_ay = int(r_ay) 
-	  Vx= r_ax*(.25)
-	  Vy = r_ay*(.25)
-	  vx = int(Vx)
-	  vy = int(Vy)
-	  print "Velocity x: %d" % (vx)
-	  print "Velocity y: %d" % (vy)
-	  r_mx = int(r_mx)
-	  r_my = int(r_my)
-	  print "Mag x: %d" % (r_mx)
-	  print "Mag y: %d" % (r_my)  
-	  return (axtotal, aytotal, vx, vy, r_mx, r_my)
-	  
-        
-       # sleep(1) # Output is fun to watch if this is commented out
+	print '[(Accelerometer X, Y, Z), (Magnetometer X, Y, Z, orientation)]'
+	for i in xrange(1,5):
+		output = lsm.read()
+		axlast = output[0]
+		aylast = output[1]
+		azlast = output[2]
+		mxlast = output[3]
+		mylast = output[4]
+		mzlast = output[5]
+		ax1 = ay1 = mx1 = my1 = 0
+		axtotal = aytotal = mxtotal = mytotal = 0
+		
+		sleep(.05)
+		newOut = lsm.read()
+		mx = newOut[3]
+		my = newOut[4]
+		mx1 = mx - mxlast
+		my1 = my - mylast
+		mxtotal = mxtotal + mx1
+		mytotal = mytotal + my1
+		
+		ax= newOut[0]
+		ay = newOut[1]
+		ax1 = ax-axlast
+		ay1 = ay-aylast
+		axtotal = axtotal + ax1
+		aytotal = aytotal + ay1
+		
+		r_mx = int(mxtotal/5)
+		r_my = int(mytotal/5)
+		r_ax = int(axtotal/5)
+		r_ay = int(aytotal/5)
+		vx = int(r_ax*(.25))
+		vy = int(r_ay*(.25))
+		
+		print "Velocity x: %d" % (vx)
+		print "Velocity y: %d" % (vy)
+		print "Mag x: %d" % (r_mx)
+		print "Mag y: %d" % (r_my)  
+		return (axtotal, aytotal, vx, vy, r_mx, r_my)
+		  
+# sleep(1) # Output is fun to watch if this is commented out
+
+
+
