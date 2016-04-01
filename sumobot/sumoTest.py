798bcc9c4ca5de7dbcb530df129428f92f04c819
zltoigo <zoe.toigo@pitt.edu>
2016-03-31 21:53:14 -0400

    Update sumoTest.py

--- sumobot/sumoTest.py
+++ sumobot/sumoTest.py
@@ -2,58 +2,98 @@ from lineSense2 import lineSensor_Class
 from longrangemethod import longrange_Class
 from shortrangemethod import shortrange_Class
 from minisumo_motorcontrol2 import Motors_Class
+from minisumo_motorcontrol3 import Motors_Class2
 from accelerometer import Accel
 	
-lineSensors= lineSensor_Class()
-motors= Motors_Class()
+#lineSensors= lineSensor_Class()
+motor1= Motors_Class()
+motor2 = Motors_Class2()
 longrange= longrange_Class()
 shortrange= shortrange_Class()
 
 short = shortrange.rngsens
-longone = longrange.rangesens
-l1 = lineSensors.check1()
+long = longrange.rangesens
+#l1 = lineSensors.check1()
 # l2 = lineSensors.check2()
 # l3 = lineSensors.check3()
-l4 = lineSensors.check4()
+#l4 = lineSensors.check4()
 
+#create file
+file = open("acceldata.csv", "w")
+
+def moving(dir):
+	if dir = 'x':
+		motors.motor_move('x', 0)
+		motors2.motor_move('x', 0)
+	if dir = 'a':
+		motors.motor_move('a', 4)
+		motors2.motor_move('a',4)
+	if dir = 's':
+		motors.motor_move('s',4)
+		motors2.motor_move('s',4)
+	if dir = 'd':
+		motors.motor_move('d', 4)
+		motors2.motor_move('d',4)
+	if dir = 'w':
+		motors.motor_move('w',4)
+		motors2.motor_move('w', 4)
+		
+	
 x0 = 0
 y0 = 0
+
+press = 'w'
+moving(press)
+
+
+
 #DRIVE FORWARD TEST LOOP
 while True:
-	motors.motor_move('w', 4)
-	# if l1 == 1: #if senses front line sensor.
-		# motors.motor_move('x', 0)
-		# break
-	if short > 0 and short < 15:
-		print('short = ', short)
-		motors.motor_move('x', 0)
-		time.sleep(2)
-		motor.motor_move('w', 4)
-	if long > 10 and long < 80:
-		print('long = ', long)
-		motors.motor_move('x', 0)
-		time.sleep(2)
-		motor.motor_move('w', 4)
-    ax, ay, x0, y0 = Accel.mag_accel(x0, y0)
-	print ('acceleration: ', ax0, '\t', ay0)
+	motor1.motor_move('d', 4)
+	motor2.motor_move('d',4)
+	# if press = 'w'
+		# motor1.motor_move('w', 4)
+		# motor2.motor_move('w', 4)
+	
+		# # if l1 == 1: #if senses front line sensor.
+			# # motors.motor_move('x', 0)
+			# # break
+		# # if short > 0 and short < 15:
+			# # print('short = ', short)
+			# # motor1.motor_move('x', 0)
+			# # motor2.motor_move('x', 0)
+			# # time.sleep(2)
+			# # motor1.motor_move('w', 4)
+			# # motor2.motor_move('w', 4)
+		# # if long > 10 and long < 80:
+			# # print('long = ', long)
+			# # motor1.motor_move('x', 0)
+			# # motor2.motor_move('x', 0)
+			# # time.sleep(2)
+			# # mmotor1.motor_move('x', 0)
+			# # motor2.motor_move('x', 0)
+	ax, ay, x0, y0 = Accel.mag_accel(x0, y0)
+	# print ('acceleration: ', ax0, '\t', ay0)
+	file.write("Ax: %d\tAy:%d")%(ax, ay)
+	# elif input = 's'
 
-# DRIVE BACKWARD TEST
-while True:
-	# if l4 == 1:
-		# motors.motor_move('x', 0)
-		# break
-    if short > 0 and short < 15:
-        print('short = ', short)
-        motors.motor_move('x', 0)
-        time.sleep(2)
-        motor.motor_move('s', 4)
-    if long > 10 and long < 80:
-        print('long = ', long)
-        motors.motor_move('x', 0)
-        time.sleep(2)
-        motor.motor_move('s', 4)
-    ax, ay, x0, y0 = Accel.mag_accel(x0, y0)
-    print ('acceleration: ', ax, '\t', ay)
+# # DRIVE BACKWARD TEST
+# while True:
+	# # if l4 == 1:
+		# # motors.motor_move('x', 0)
+		# # break
+    # if short > 0 and short < 15:
+        # print('short = ', short)
+        # motors.motor_move('x', 0)
+        # time.sleep(2)
+        # motor.motor_move('s', 4)
+    # if long > 10 and long < 80:
+        # print('long = ', long)
+        # motors.motor_move('x', 0)
+        # time.sleep(2)
+        # motor.motor_move('s', 4)
+    # ax, ay, x0, y0 = Accel.mag_accel(x0, y0)
+    # print ('acceleration: ', ax, '\t', ay)
 
 
 
