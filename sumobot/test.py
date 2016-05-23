978f959a09abaf2d0d39e9cb6d86ee8970b62121
vina7 <van7@pitt.edu>
2016-05-22 21:14:03 -0400

    remove folders from repository

--- sumobot/test.py
+++
@@ -1,9 +0,0 @@
-from minisumo_motorcontrol2 import Motors_Class
-from minisumo_motorcontrol3 import Motors_Class2
-
-motors=Motors_Class()
-motors2=Motors_Class2()
-while(1):
-	direct = raw_input()
-	motors.motor_move(direct,6)
-        motors2.motor_move(direct,6)
